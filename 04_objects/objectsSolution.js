a8c65081736cd2e259f5a5e5e741070256ae7fa6
Sumit Paul <pauls1317@gmail.com>
2024-07-19 22:28:28 +0530

    solution

---
+++ 04_objects/objectsSolution.js
@@ -0,0 +1,28 @@
+var dune = {
+    title: "Dune",
+    originalTitle: "Dune Part One",
+    director: "Denis Villeneuve",
+    releaseYear: 2021,
+    boxOffice: {
+        budget: 165000000,
+        grossUS: 108327830,
+        grossWorldwide: 400671789,
+    },
+};
+var cats = {
+    title: "Cats",
+    director: "Tom Hooper",
+    releaseYear: 2019,
+    boxOffice: {
+        budget: 95000000,
+        grossUS: 27166770,
+        grossWorldwide: 73833348,
+    },
+};
+// Write a function called getProfit that accepts a single Movie object
+// It should return the movie's worldwide gross minus its budget
+function getProfit(movie) {
+    return movie.boxOffice.grossWorldwide - movie.boxOffice.budget;
+}
+// For example...
+console.log(getProfit(cats)); // => -21166652
