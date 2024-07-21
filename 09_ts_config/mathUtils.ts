8aa1c6181a0163c430b3bb8fc61f3926869ac16b
Sumit Paul <pauls1317@gmail.com>
2024-07-21 22:17:21 +0530

    strict type-checking

--- 09_ts_config/mathUtils.ts
+++ 09_ts_config/mathUtils.ts
@@ -3,3 +3,9 @@ function calcAge(yearOfBirth: number): number {
 }
 
 console.log(calcAge(1998)); // 26 -> 2024 - 1998
+
+
+// Implicit any type checking 
+// const getSum(num1, num2) {
+//     return num1 + num2;
+// }
