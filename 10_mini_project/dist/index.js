d1ea1a71b6d4fe1186d3843eef677338bdd303be
Sumit Paul <pauls1317@gmail.com>
2024-07-23 10:00:25 +0530

    create new todo using document API

--- 10_mini_project/dist/index.js
+++ 10_mini_project/dist/index.js
@@ -8,24 +8,34 @@ console.log(btn);
 //* Non-null assertion operator (!)
 const btn2 = document.getElementById("btn");
 btn2.addEventListener("click", function () {
-    alert("You clicked me!!");
+    // alert("You clicked me!!");
 });
 //* Type assertion - 'as' keyword
-const mystery = 'Mystery';
+const mystery = "Mystery";
 // mystery.length; // ERROR: 'mystery' is of type 'unknown'.
-// asserting type - 
+// asserting type -
 mystery.length;
 //* Type assertion with DOM
-const clickBtn = document.getElementById('btn');
-const inputElement = document.getElementById('todoInput');
+const clickBtn = document.getElementById("btn");
+const inputElement = document.getElementById("todoInput");
 // clickBtn.addEventListener('click', function(){
 //   alert(inputElement.value);
 //   inputElement.value = "";
 // })
-const todoForm = document.querySelector('form');
+const todoForm = document.querySelector("form");
+const todoList = document.getElementById("todoList");
 //* Working with Events - submit event
 function handleSubmit(e) {
     e.preventDefault();
-    console.log('Submitted!!');
+    const todoText = inputElement.value;
+    const newTodoElement = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    // newTodoElement.innerText = todoText;
+    // OR
+    newTodoElement.append(todoText);
+    newTodoElement.append(checkbox);
+    todoList.append(newTodoElement);
+    inputElement.value = "";
 }
 todoForm.addEventListener('submit', handleSubmit);
