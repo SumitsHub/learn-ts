99d3e646481e51a83eeef592be043a0aa59a436b
Sumit Paul <pauls1317@gmail.com>
2024-07-23 08:54:38 +0530

    Type assertion with DOM using 'as' keyword

--- 10_mini_project/dist/index.js
+++ 10_mini_project/dist/index.js
@@ -5,8 +5,20 @@ console.log(btn);
 //   //* 'btn' is possibly 'null'
 //   alert("Clicked");
 // });
-//* Non-null assertion operator
+//* Non-null assertion operator (!)
 const btn2 = document.getElementById("btn");
 btn2.addEventListener("click", function () {
     alert("You clicked me!!");
+});
+//* Type assertion - 'as' keyword
+const mystery = 'Mystery';
+// mystery.length; // ERROR: 'mystery' is of type 'unknown'.
+// asserting type - 
+mystery.length;
+//* Type assertion with DOM
+const clickBtn = document.getElementById('btn');
+const inputElement = document.getElementById('todoInput');
+clickBtn.addEventListener('click', function () {
+    alert(inputElement.value);
+    inputElement.value = "";
 });
