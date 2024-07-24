8b3dadcc02fed692d3ef9db69abfeb333a221002
Sumit Paul <pauls1317@gmail.com>
2024-07-24 23:04:40 +0530

    private fields

--- 11_classes_recap/dist/index.js
+++ 11_classes_recap/dist/index.js
@@ -1,6 +1,10 @@
 class Person {
   // class variables - it's same as instance variable, it's not a static variable
   noOfWins = 0;
+
+  // private variable - this is accessible inside class only
+  #secret = "Secret message";
+
   constructor(name, age) {
     this.name = name;
     this.age = age;
@@ -12,8 +16,18 @@ class Person {
     );
   }
 
+  tellSecret() {
+    // private variable is accessible from inside class
+    console.log(`My secret is ${this.#secret}.`);
+  }
+
   incWin() {
     this.noOfWins++;
+  }
+
+  // private method - this is accessible from inside class only
+  #privateMethod() {
+    console.log("This is a private method.");
   }
 }
 
@@ -21,6 +35,12 @@ const person1 = new Person("Alice", 30);
 person1.greet(); // Output: Hello, my name is Alice and I am 30 years old.
 person1.incWin();
 console.log(person1.noOfWins); // 1
+
+// person1.#secret; // SyntaxError: Private field '#secret' must be declared in an enclosing class
+// person1.#privateMethod(); // SyntaxError: Private field '#privateMethod' must be declared in an enclosing class
+
+// Trying to access non existing random variable - it gives same error as if this private variable exists
+person1.#randomVar; // SyntaxError: Private field '#randomVar' must be declared in an enclosing class
 
 const person2 = new Person("Bob", 25);
 console.log(person2.age); // Output: 25
