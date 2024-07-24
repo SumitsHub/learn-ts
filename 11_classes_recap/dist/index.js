68901b05f666b770adce173dc1ee93979fbad929
Sumit Paul <pauls1317@gmail.com>
2024-07-24 23:27:12 +0530

    static variables + getter and setter

--- 11_classes_recap/dist/index.js
+++ 11_classes_recap/dist/index.js
@@ -2,12 +2,32 @@ class Person {
   // class variables - it's same as instance variable, it's not a static variable
   noOfWins = 0;
 
+  // static variable - class level variable cannot be accessed using instance variable
+  static species = "HUMAN";
+
   // private variable - this is accessible inside class only
   #secret = "Secret message";
 
   constructor(name, age) {
     this.name = name;
     this.age = age;
+  }
+
+  // defining getter
+  get birthYear() {
+    return new Date().getFullYear() - this.age;
+  }
+
+  set birthYear(year) {
+    if (year < 0) {
+      throw new Error("Year cant be negative!");
+    }
+    this.yearOfBirth = year;
+  }
+
+  // static variable
+  static createNewPerson() {
+    return new Person("Andy", "Marten");
   }
 
   greet() {
@@ -40,8 +60,22 @@ console.log(person1.noOfWins); // 1
 // person1.#privateMethod(); // SyntaxError: Private field '#privateMethod' must be declared in an enclosing class
 
 // Trying to access non existing random variable - it gives same error as if this private variable exists
-person1.#randomVar; // SyntaxError: Private field '#randomVar' must be declared in an enclosing class
+// person1.#randomVar; // SyntaxError: Private field '#randomVar' must be declared in an enclosing class
 
 const person2 = new Person("Bob", 25);
 console.log(person2.age); // Output: 25
 console.log(person2.noOfWins); // 0
+
+// using getter - as property
+console.log(person1.birthYear); // 1994
+
+// setting year using setter
+person1.birthYear = 1993;
+console.log(person1.birthYear);
+
+// static fields -
+console.log(Person.species); // HUMAN
+console.log(person1.species); // undefined
+
+console.log(Person.createNewPerson());
+// console.log(person1.createNewPerson()); // ERROR: it's not a function
