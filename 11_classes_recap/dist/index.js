7bcfcc8ddf2f697f42712ebb5368764d8af7ba0a
Sumit Paul <sumitpaul.works@gmail.com>
2024-07-24 20:16:15 +0530

    class basics

--- 11_classes_recap/dist/index.js
+++ 11_classes_recap/dist/index.js
@@ -1,2 +1,18 @@
-"use strict";
-console.log('Hello, TypeScript!!!');
+class Person {
+    // class variables
+  constructor(name, age) {
+	this.name = name;
+	this.age = age;
+  }
+
+  greet() {
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+}
+
+const person1 = new Person('Alice', 30);
+person1.greet(); // Output: Hello, my name is Alice and I am 30 years old.
+
+
+const person2 = new Person('Bob', 25);
+console.log(person2.age); // Output: 25
