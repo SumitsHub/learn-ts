47228390c6ccb198ffb841231a1a3be2d3d35bfa
Sumit Paul <pauls1317@gmail.com>
2024-07-25 09:36:57 +0530

    parameter properties shorthand

--- 11_classes_recap/dist/index.js
+++ 11_classes_recap/dist/index.js
@@ -1,7 +1,18 @@
 "use strict";
 var _Player_netWorth;
 class Player {
+    //   constructor(first: string, last: string) {
+    //     this.first = first;
+    //     this.last = last;
+    //   this.nickName = last;
+    //   this.sport = "Football";
+    //   }
+    //* parameters properties shorthand
     constructor(first, last) {
+        this.first = first;
+        this.last = last;
+        //   first: String;
+        //   last: string;
         // class variable - useful if we need to initialize value for all instance variables
         // score: number = 0;
         this.score = 0; // type inferred to number
@@ -11,8 +22,6 @@ class Player {
         this.netWorth = 120000;
         // OR - use JS syntax
         _Player_netWorth.set(this, 120000);
-        this.first = first;
-        this.last = last;
         this.nickName = last;
         this.sport = "Football";
     }
@@ -20,5 +29,6 @@ class Player {
 _Player_netWorth = new WeakMap();
 const elton = new Player("Elton", "Steele");
 // elton.nickName = 'Something'; // ERROR- Cannot assign to 'nickName' because it is a read-only property.
+console.log(elton);
 // elton.netWorth; // ERROR - Property 'netWorth' is private and only accessible within class 'Player'.
 // elton.#netWorth; // ERROR- not accessible outside of the class
--- 11_classes_recap/src/index.ts
+++ 11_classes_recap/src/index.ts
@@ -1,6 +1,6 @@
 class Player {
-  first: String;
-  last: string;
+  //   first: String;
+  //   last: string;
 
   // class variable - useful if we need to initialize value for all instance variables
   // score: number = 0;
@@ -17,10 +17,17 @@ class Player {
   // OR - use JS syntax
   #netWorth = 120000;
 
-  constructor(first: string, last: string) {
-    this.first = first;
-    this.last = last;
+  //   constructor(first: string, last: string) {
+  //     this.first = first;
+  //     this.last = last;
 
+  //   this.nickName = last;
+
+  //   this.sport = "Football";
+  //   }
+
+  //* parameters properties shorthand
+  constructor(public first: string, public last: string) {
     this.nickName = last;
 
     this.sport = "Football";
@@ -29,6 +36,8 @@ class Player {
 
 const elton = new Player("Elton", "Steele");
 // elton.nickName = 'Something'; // ERROR- Cannot assign to 'nickName' because it is a read-only property.
+
+console.log(elton);
 
 // elton.netWorth; // ERROR - Property 'netWorth' is private and only accessible within class 'Player'.
 // elton.#netWorth; // ERROR- not accessible outside of the class
