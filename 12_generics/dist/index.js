b6edb6d3ca0613b81dcf4cae343e51f4f136bbf2
Sumit Paul <sumitpaul.works@gmail.com>
2024-07-25 15:13:32 +0530

    intro to generics and generic function

--- 12_generics/dist/index.js
+++ 12_generics/dist/index.js
@@ -1,15 +1,37 @@
 "use strict";
 // array of numbers - type bound to number
 // const nums: number[] = [];
-//* Examples 
+//* Examples
 //* #01. Array - creating array of numbers and strings using generics
 const nums = [];
 const colors = [];
 //* #02. querySelector - returns Element | null
-const inpuEl = document.querySelector('#username');
+const inpuEl = document.querySelector("#username");
 // OR
 // const inpuEl = document.querySelector('#username') as HTMLInputElement;
 console.dir(inpuEl);
 console.log(inpuEl.value);
-const btnEl = document.querySelector('btn');
+const btnEl = document.querySelector("btn");
 console.log(btnEl.textContent);
+//* creating own generic function
+function createArray(length, value) {
+    return new Array(length).fill(value);
+}
+// creating string array
+const arr = createArray(5, "hello");
+console.log(arr);
+// creating number array
+const arr2 = createArray(5, 10);
+console.log(arr2); // [10, 10, 10, 10, 10]
+function randomElement(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+}
+// using string array
+const randomStr = randomElement(["hello", "world", "typescript"]);
+console.log(randomStr); // hello | world | typescript
+const randomNum = randomElement([10, 20, 30, 40, 50]);
+console.log(randomNum); // 
+//* inferred generic type parameter
+console.log(randomElement(["hello", "world", "typescript"])); // TypeScript will infer the type of T as string
+console.log(randomElement([true, false, true])); // TypeScript will infer the type of T as boolean
--- 12_generics/src/index.ts
+++ 12_generics/src/index.ts
@@ -1,18 +1,45 @@
 // array of numbers - type bound to number
 // const nums: number[] = [];
 
-//* Examples 
+//* Examples
 
 //* #01. Array - creating array of numbers and strings using generics
 const nums: Array<number> = [];
 const colors: Array<string> = [];
 
 //* #02. querySelector - returns Element | null
-const inpuEl = document.querySelector<HTMLInputElement>('#username')!;
+const inpuEl = document.querySelector<HTMLInputElement>("#username")!;
 // OR
 // const inpuEl = document.querySelector('#username') as HTMLInputElement;
 console.dir(inpuEl);
 console.log(inpuEl.value);
 
-const btnEl = document.querySelector<HTMLButtonElement>('btn')!;
+const btnEl = document.querySelector<HTMLButtonElement>("btn")!;
 console.log(btnEl.textContent);
+
+//* creating own generic function
+function createArray<T>(length: number, value: T): Array<T> {
+  return new Array(length).fill(value);
+}
+// creating string array
+const arr = createArray<string>(5, "hello");
+console.log(arr);
+// creating number array
+const arr2 = createArray<number>(5, 10);
+console.log(arr2); // [10, 10, 10, 10, 10]
+
+function randomElement<T>(arr: T[]): T {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+// using string array
+const randomStr = randomElement<string>(["hello", "world", "typescript"]);
+console.log(randomStr); // hello | world | typescript
+const randomNum = randomElement<number>([10, 20, 30, 40, 50]);
+console.log(randomNum); // 
+
+
+//* inferred generic type parameter
+console.log(randomElement(["hello", "world", "typescript"])); // TypeScript will infer the type of T as string
+console.log(randomElement([true, false, true])); // TypeScript will infer the type of T as boolean
+
