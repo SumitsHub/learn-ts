2aafb7c8ba8a130293206720481fd1df1fa0c225
Sumit Paul <pauls1317@gmail.com>
2024-07-25 09:30:56 +0530

    public and private modifiers

--- 11_classes_recap/dist/index.js
+++ 11_classes_recap/dist/index.js
@@ -1,11 +1,24 @@
 "use strict";
+var _Player_netWorth;
 class Player {
     constructor(first, last) {
         // class variable - useful if we need to initialize value for all instance variables
         // score: number = 0;
         this.score = 0; // type inferred to number
+        //* public property - can be accessed from anywhere - it's just convention for developers don't add anything extra
+        this.someVar = "Something";
+        //* Declaring private variable
+        this.netWorth = 120000;
+        // OR - use JS syntax
+        _Player_netWorth.set(this, 120000);
         this.first = first;
         this.last = last;
+        this.nickName = last;
+        this.sport = "Football";
     }
 }
+_Player_netWorth = new WeakMap();
 const elton = new Player("Elton", "Steele");
+// elton.nickName = 'Something'; // ERROR- Cannot assign to 'nickName' because it is a read-only property.
+// elton.netWorth; // ERROR - Property 'netWorth' is private and only accessible within class 'Player'.
+// elton.#netWorth; // ERROR- not accessible outside of the class
--- 11_classes_recap/src/index.ts
+++ 11_classes_recap/src/index.ts
@@ -6,10 +6,29 @@ class Player {
   // score: number = 0;
   score = 0; // type inferred to number
 
+  // readonly modifier
+  readonly nickName: string;
+  //* public property - can be accessed from anywhere - it's just convention for developers don't add anything extra
+  public someVar = "Something";
+  public readonly sport: string;
+
+  //* Declaring private variable
+  private netWorth: number = 120000;
+  // OR - use JS syntax
+  #netWorth = 120000;
+
   constructor(first: string, last: string) {
     this.first = first;
     this.last = last;
+
+    this.nickName = last;
+
+    this.sport = "Football";
   }
 }
 
 const elton = new Player("Elton", "Steele");
+// elton.nickName = 'Something'; // ERROR- Cannot assign to 'nickName' because it is a read-only property.
+
+// elton.netWorth; // ERROR - Property 'netWorth' is private and only accessible within class 'Player'.
+// elton.#netWorth; // ERROR- not accessible outside of the class
