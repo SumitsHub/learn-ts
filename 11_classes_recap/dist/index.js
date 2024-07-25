1414940aab9bc3d81fa247bc188e19d9eb457ad7
Sumit Paul <pauls1317@gmail.com>
2024-07-25 09:12:40 +0530

    class in ts & class variable

--- 11_classes_recap/dist/index.js
+++ 11_classes_recap/dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('Hello, TypeScript!!!');
+class Player {
+    constructor(first, last) {
+        // class variable - useful if we need to initialize value for all instance variables
+        // score: number = 0;
+        this.score = 0; // type inferred to number
+        this.first = first;
+        this.last = last;
+    }
+}
+const elton = new Player("Elton", "Steele");
--- 11_classes_recap/src/index.ts
+++ 11_classes_recap/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, TypeScript!!!');
+class Player {
+  first: String;
+  last: string;
+
+  // class variable - useful if we need to initialize value for all instance variables
+  // score: number = 0;
+  score = 0; // type inferred to number
+
+  constructor(first: string, last: string) {
+    this.first = first;
+    this.last = last;
+  }
+}
+
+const elton = new Player("Elton", "Steele");
