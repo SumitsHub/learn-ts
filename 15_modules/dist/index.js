5a9903522ecf3d6935f9d15050041a309dd1b59c
Sumit Paul <pauls1317@gmail.com>
2024-07-28 17:28:14 +0530

    using es6 modules

--- 15_modules/dist/index.js
+++ 15_modules/dist/index.js
@@ -1,7 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // import { add, getRandomElement } from "./utils.ts"; // ERROR: An import path can only end with a '.ts' extension when 'allowImportingTsExtensions' is enabled.
-const utils_js_1 = require("./utils.js");
+import { add, getRandomElement } from "./utils.js";
 // using import statement will make TS to consider ts files as module
-console.log((0, utils_js_1.add)(3, 4));
-console.log((0, utils_js_1.getRandomElement)(["Ram", "Sam", "John", "Steve"]));
+console.log(add(3, 4));
+console.log(getRandomElement(["Ram", "Sam", "John", "Steve"]));
--- 15_modules/dist/utils.js
+++ 15_modules/dist/utils.js
@@ -1,12 +1,7 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getRandomElement = void 0;
-exports.add = add;
-function add(x, y) {
+export function add(x, y) {
     return x + y;
 }
-const getRandomElement = (arr) => {
+export const getRandomElement = (arr) => {
     const index = Math.floor(Math.random() * arr.length);
     return arr[index];
 };
-exports.getRandomElement = getRandomElement;
