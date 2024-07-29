d4fc39b0a9e9e088e8fe9c1106178f1a76118222
Sumit Paul <pauls1317@gmail.com>
2024-07-29 23:23:11 +0530

    webpack config for production

---
+++ 16_webpack_ts/webpack.prod.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+  },
+};
