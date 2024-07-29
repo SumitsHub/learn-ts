6de36c7ee07d66f4343c08d5e3bed6062259e532
Sumit Paul <pauls1317@gmail.com>
2024-07-29 23:24:46 +0530

    contenthash

--- 16_webpack_ts/webpack.prod.js
+++ 16_webpack_ts/webpack.prod.js
@@ -22,7 +22,7 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
+    filename: "[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/dist",
   },
