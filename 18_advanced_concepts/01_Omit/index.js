f64e7d6142990094091bd86e4896dfc33cb3586c
Sumit Paul <sumitpaul.works@gmail.com>
2024-08-08 18:09:40 +0530

    initialized tsconfig using tsc --init

---
+++ 18_advanced_concepts/01_Omit/index.js
@@ -0,0 +1,16 @@
+/** Omit in TypeScript */
+var person = {
+    name: "John Doe",
+    age: 30,
+    address: "123 Main St",
+    // email is omitted
+};
+function updateUserInfo(user) {
+    // Function implementation
+}
+updateUserInfo({
+    username: "newUsername",
+    password: "newPassword",
+    email: "newEmail@example.com",
+    // id is omitted
+});
